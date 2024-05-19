fecbfac130c6e5f99b8c9ce417f7c9a556c92ebc
Ved Sharma <vedsharma8877@gmail.com>
2024-05-19 23:32:48 +0530

    done with the project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api').then(res => setPeople(res.data))
+  }, [])
+  
+  return people.map((p, index) => {
+    return <p key={index}>{p.id} {p.name} {p.age}</p>
+  })
 }
 
 export default App;
